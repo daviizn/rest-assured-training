cbc3b8d9f76549df6a40c529b6ce5f0eb879a66c
Davi Serra <daviserrafc@gmail.com>
2025-08-30 18:40:54 -0300

    🟢 feat(contas): adicionar teste de incluir conta com sucesso

---
+++ src/main/java/dev/davizn/rest/tests/BarrigaTest.java
@@ -0,0 +1,47 @@
+package dev.davizn.rest.tests;
+
+import dev.davizn.rest.core.BaseTest;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class BarrigaTest extends BaseTest {
+
+    @Test
+    public void naoDeveAcessarAPISemToken() {
+        given()
+        .when()
+            .get("/contas")
+        .then()
+            .statusCode(401)
+        ;
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso() {
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "davizn@dev");
+        login.put("senha", "123456");
+
+        String token = given()
+            .body(login)
+        .when()
+            .post("/signin")
+        .then()
+            .statusCode(200)
+            .extract().path("token");
+
+        given()
+            .header("Authorization", "JWT " + token)
+            .body("{\"nome\": \"conta qualquer\"}")
+        .when()
+            .post("/contas")
+        .then()
+            .statusCode(201)
+        ;
+
+    }
+}
